c3869fd04abccecff1179e67b495f3d5770f33c2
roderick romero <rjrr@gmail.com>
2022-08-31 22:39:52 -0500

    add feature plausible analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script id="plausible-analytics" src="https://plausible.roderickromero.dev/js/plausible.js" />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
