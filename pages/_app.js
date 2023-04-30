6b29746d8ea5f93c45ed92af0a1b7b1a623edc5a
roderick romero <rjrr@gmail.com>
2023-04-30 13:50:31 -0500

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import Script from "next/script";
 
-function MyApp({ Component, pageProps }) {
+import { Analytics } from '@vercel/analytics/react';
+
+function MyApp ({ Component, pageProps }) {
 	return (
 		<>
 			<Script id="plausible-analytics" src="https://plausible.roderickromero.dev/js/plausible.js" />
 			<Component {...pageProps} />
+			<Analytics />
 		</>
 	);
 }
